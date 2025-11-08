ca581e2acc4ed83e78336220edd7d7affae14b1e
고산하 <phill.ko@kurlycorp.com>
2025-11-08 23:10:57 +0900

    Initialize project structure with Spring Boot modules, Gradle configuration, and Snowflake ID generator.

---
+++ service/comment/src/main/java/simple/board/comment/CommentApplication.java
@@ -0,0 +1,12 @@
+package simple.board.comment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(CommentApplication.class, args);
+    }
+}
---
+++ service/hot-article/src/main/java/simple/board/hotarticle/HotArticleApplication.java
@@ -0,0 +1,12 @@
+package simple.board.hotarticle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotArticleApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(HotArticleApplication.class, args);
+    }
+}
